1cd761a514d07637ed28fbe25705569cac3e0a45
EmmanuelNoel <jahounoel@gmail.com>
2023-06-14 12:06:23 +0200

    push

--- resources/views/statCaisse.blade.php
+++ resources/views/statCaisse.blade.php
@@ -31,6 +31,11 @@
             margin: auto;
 
 
+
+        }
+
+        .thead{
+border: 1px solid black;
         }
 
         .thead,
@@ -47,6 +52,17 @@
         .tbody{
             line-height: 80px;
             font-size: 40px;
+        }
+
+        .tbody2{
+            border: 1px dotted black;
+            height: 120px;
+            text-align: left;
+        }
+
+        .text{
+            margin-left: 60%;
+            line-height: 200%;
         }
     </style>
 
@@ -381,104 +397,37 @@
                             </td>
                             <td> 201 500</td>
                             <td> 0 </td>
-                        </tr> <tr>
-                            <td>
-                                AD ACAC
-                            </td>
-                            <td>
-                                AD ACACIA 10KG
-                            </td>
-                            <td>
-                                13,0000
-                            </td>
-                            <td>
-                                170 763
-                            </td>
-                            <td>
-                                30 737
-                            </td>
-                            <td> 201 500</td>
-                            <td> 0 </td>
-                        </tr> <tr>
-                            <td>
-                                AD ACAC
-                            </td>
-                            <td>
-                                AD ACACIA 10KG
-                            </td>
-                            <td>
-                                13,0000
-                            </td>
-                            <td>
-                                170 763
-                            </td>
-                            <td>
-                                30 737
-                            </td>
-                            <td> 201 500</td>
-                            <td> 0 </td>
-                        </tr> <tr>
-                            <td>
-                                AD ACAC
-                            </td>
-                            <td>
-                                AD ACACIA 10KG
-                            </td>
-                            <td>
-                                13,0000
-                            </td>
-                            <td>
-                                170 763
-                            </td>
-                            <td>
-                                30 737
-                            </td>
-                            <td> 201 500</td>
-                            <td> 0 </td>
-                        </tr> <tr>
-                            <td>
-                                AD ACAC
-                            </td>
-                            <td>
-                                AD ACACIA 10KG
-                            </td>
-                            <td>
-                                13,0000
-                            </td>
-                            <td>
-                                170 763
-                            </td>
-                            <td>
-                                30 737
-                            </td>
-                            <td> 201 500</td>
-                            <td> 0 </td>
-                        </tr>
-
-                        <tr>
-                           <span>
-                            MP SPP
-                           </span>
-                           <span>
-                            Edité par eFactEl
-                           </span>
-                           <span>
-                            le
-                           </span>
-                           <span>
-
-                            31/05/2023
-                           </span>
-                           <span>
-                            17:26:33
-                           </span>
-                           <span>
-                            1/2
-                           </span>
-                        </tr>
+                        </tr> 
 
                     </tbody>
-                </table>
+                </table> <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <div style="border: 1px solid black; height: 120px; padding-top: 30px;">
+
+                        <span style="margin-left:10px;">
+                         MP SPP
+                        </span>
+                        <span style="margin-left:800px;">
+                         Edité par eFactEl
+                        </span>
+                        <span style="margin-left:20px;">
+                         le
+                        </span>
+                        <span>
+
+                         31/05/2023
+                        </span>
+                        <span style="margin-left:30px;">
+                         17:26:33
+                        </span>
+                        <span style="margin-left:800px;">
+                         1/2
+                        </span>
+
+                </div>
             </div>
 
         </div>
@@ -520,7 +469,7 @@
                 </div>
 
                 <table>
-                    <thead class="thead">
+                    <thead class="thead" >
                         <tr>
                             <td>
                                 Reference
@@ -548,142 +497,56 @@
                             </td>
                         </tr>
                     </thead>
-                    <tbody class="tbody">
+                    <tbody class="tbody  " >
 
-                        <tr>
+                        <tr style="border: 1px dotted black;">
                             <td>
-                                AD ACAC
+
                             </td>
                             <td>
-                                AD ACACIA 10KG
+
                             </td>
-                            <td>
-                                13,0000
+                            <td class="tbody2">
+                                256,0000
+                            <td class="tbody2">
+                                4 539 588
                             </td>
-                            <td>
-                                170 763
+                            <td class="tbody2">
+                                122 812
                             </td>
-                            <td>
-                                30 737
-                            </td>
-                            <td> 201 500</td>
-                            <td> 0 </td>
+                            <td class="tbody2"> 4 662 400</td>
+                            <td class="tbody2"> 0 </td>
                         </tr>
-                        <tr>
-                            <td>
-                                AD ACAC
-                            </td>
-                            <td>
-                                AD ACACIA 10KG
-                            </td>
-                            <td>
-                                13,0000
-                            </td>
-                            <td>
-                                170 763
-                            </td>
-                            <td>
-                                30 737
-                            </td>
-                            <td> 201 500</td>
-                            <td> 0 </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                AD ACAC
-                            </td>
-                            <td>
-                                AD ACACIA 10KG
-                            </td>
-                            <td>
-                                13,0000
-                            </td>
-                            <td>
-                                170 763
-                            </td>
-                            <td>
-                                30 737
-                            </td>
-                            <td> 201 500</td>
-                            <td> 0 </td>
-                        </tr> <tr>
-                            <td>
-                                AD ACAC
-                            </td>
-                            <td>
-                                AD ACACIA 10KG
-                            </td>
-                            <td>
-                                13,0000
-                            </td>
-                            <td>
-                                170 763
-                            </td>
-                            <td>
-                                30 737
-                            </td>
-                            <td> 201 500</td>
-                            <td> 0 </td>
-                        </tr> <tr>
-                            <td>
-                                AD ACAC
-                            </td>
-                            <td>
-                                AD ACACIA 10KG
-                            </td>
-                            <td>
-                                13,0000
-                            </td>
-                            <td>
-                                170 763
-                            </td>
-                            <td>
-                                30 737
-                            </td>
-                            <td> 201 500</td>
-                            <td> 0 </td>
-                        </tr> <tr>
-                            <td>
-                                AD ACAC
-                            </td>
-                            <td>
-                                AD ACACIA 10KG
-                            </td>
-                            <td>
-                                13,0000
-                            </td>
-                            <td>
-                                170 763
-                            </td>
-                            <td>
-                                30 737
-                            </td>
-                            <td> 201 500</td>
-                            <td> 0 </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                AD ACAC
-                            </td>
-                            <td>
-                                AD ACACIA 10KG
-                            </td>
-                            <td>
-                                13,0000
-                            </td>
-                            <td>
-                                170 763
-                            </td>
-                            <td>
-                                30 737
-                            </td>
-                            <td> 201 500</td>
-                            <td> 0 </td>
-                        </tr> 
+
 
                     </tbody>
                 </table>
+
+                 <div class="text">
+                    <div>
+                        MontantAIB collecté :0
+                    </div>
+                    <div>
+                        Momtant des ventes :4 662 400
+                    </div>
+                    <div>
+                        Momtant des crédits :0
+                    </div>
+
+                 </div>
+
+
+                 <div style="line-height: 120px;">
+                    <span>
+                        Nombre de produits : 34
+                    </span>
+                    <span style="margin-left: 42%;">
+                        Momtant en caisse : 4 662 400
+                    </span>
+                </div>
+
             </div>
+
 
         </div>
 
